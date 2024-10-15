d77af464913853d62cc158a5838dac6c6ad21dca
Houssem Tabboubi <124980612+tb7ssem@users.noreply.github.com>
2024-10-15 10:24:16 +0100

    first commit

---
+++ index.ts
@@ -0,0 +1,23 @@
+interface Vehicle {
+    make: string;
+    model: string;
+    year: number;
+    start(): void;
+  }
+  class Car implements Vehicle {
+    make: string;
+    model: string;
+    year: number;
+  
+    constructor(make: string, model: string, year: number) {
+      this.make = make;
+      this.model = model;
+      this.year = year;
+    }
+  
+    start(): void {
+      console.log("Car engine started");
+    }
+  }
+  const myCar = new Car("Toyota", "Corolla", 2022);
+  myCar.start(); // Output: "Car engine started"
